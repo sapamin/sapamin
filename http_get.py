63f8c9e9216eaea48aa69cffb01e6c73820a068f
sapamin <bcn5186@g.cbnu.ac.kr>
2021-09-21 15:18:49 +0900

    google map http get

---
+++ http_get.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+import json
+input=sys.stdin.readline
+print("장소 검색:",end="")
+place=str(input())
+search='https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBLMbHO69BPF2HNdQ1YT72pKEHLjUcvVLw' \
+'&sensor=false&language=ko&address={}'.format(place)
+
+response=requests.get(search)
+
+results=response.json()
+lat=results['results'][0]['geometry']['location']['lat']
+lng=results['results'][0]['geometry']['location']['lng']
+adress=results['results'][0]['formatted_address']
+
+print("위도:",lat)
+print("경도:",lng)
+print("주소:",adress)
+
+
